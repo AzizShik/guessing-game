3c8d8644e7f37fdbbd75d528ce7e7629d4b61c73
Abdyl-Aziz <azizshihabov@mail.ru>
2025-08-30 22:36:23 +0300

    feat: add solution for the task

--- src/index.js
+++ src/index.js
@@ -1,16 +1,31 @@
 class GuessingGame {
   constructor() {
-    throw new Error('Not implemented');
+    this.min = 0;
+    this.max = 0;
+    this.mid = 0;
   }
-  /*
-  setRange(min, max) { }
 
-  guess() { }
+  setRange(min, max) {
+    this.min = min;
+    this.max = max;
+  }
 
-  lower() { }
+  guess() {
+    this.mid = Math.round((this.min + this.max) / 2);
+    return this.mid;
+  }
 
-  greater() { }
-*/
+  lower() {
+    this.max = this.mid;
+    this.mid = Math.round((this.min + this.max) / 2);
+    return this.mid;
+  }
+
+  greater() {
+    this.min = this.mid;
+    this.mid = Math.round((this.min + this.max) / 2);
+    return this.mid;
+  }
 }
 
 module.exports = GuessingGame;
